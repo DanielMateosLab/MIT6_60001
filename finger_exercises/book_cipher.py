cebc8a30e5b593df459c6ef473a7c912eadfdf48
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-12-31 10:03:00 +0100

    Avoid use of -1 indexes in gen_decode_keys

--- finger_exercises/book_cipher.py
+++ finger_exercises/book_cipher.py
@@ -1,5 +1,6 @@
 from typing import Any, Callable
 
+coding_dictionary = 'Somewhere in La Mancha, in a place whose name I do not care to remember, a gentleman lived not long ago, one of those who has a lance and ancient shield on a shelf and keeps a skinny nag and a greyhound for racing'
 
 gen_code_keys: Callable[[str, str], Any] = lambda book, plain_text:(
   {c: str(book.find(c)) for c in plain_text})
@@ -11,10 +12,9 @@ encoder: Callable[[dict[str, str], str], str] = (
 encrypt: Callable[[str, str], str] = (lambda book, plain_text:
   encoder(gen_code_keys(book, plain_text), plain_text))
 
-coding_dictionary = 'Once upon a time, in a house in a land far away'
+gen_decode_keys: Callable[[str, str], Any] = (lambda book, cipher_text:
+  {s: book[int(s)] for s in cipher_text.split("*")[1:] if int(s) > 0})
 
-gen_decode_keys: Callable[[str, str], dict[str, str]] = (lambda book, cipher_text:
-  {s: book[int(s)] for s in cipher_text.split('*')})
-
-encrypted_message = encrypt(coding_dictionary, 'no is no')
+encrypted_message = encrypt(coding_dictionary, 'No is no')
+print(encrypted_message)
 print(gen_decode_keys(coding_dictionary, encrypted_message))
