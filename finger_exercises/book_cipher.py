d45197f3a9353cd9af72ba4cc148e559f5df57aa
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-12-31 10:11:03 +0100

    Add comments and required functions declarations

--- finger_exercises/book_cipher.py
+++ finger_exercises/book_cipher.py
@@ -2,6 +2,7 @@ from typing import Any, Callable
 
 coding_dictionary = 'Somewhere in La Mancha, in a place whose name I do not care to remember, a gentleman lived not long ago, one of those who has a lance and ancient shield on a shelf and keeps a skinny nag and a greyhound for racing'
 
+# Encrypting functions
 gen_code_keys: Callable[[str, str], Any] = lambda book, plain_text:(
   {c: str(book.find(c)) for c in plain_text})
 
@@ -12,8 +13,15 @@ encoder: Callable[[dict[str, str], str], str] = (
 encrypt: Callable[[str, str], str] = (lambda book, plain_text:
   encoder(gen_code_keys(book, plain_text), plain_text))
 
+# Decrypting functions
 gen_decode_keys: Callable[[str, str], Any] = (lambda book, cipher_text:
   {s: book[int(s)] for s in cipher_text.split("*")[1:] if int(s) > 0})
+
+def decoder():
+  pass
+
+def decrypt():
+  pass
 
 encrypted_message = encrypt(coding_dictionary, 'No is no')
 print(encrypted_message)
