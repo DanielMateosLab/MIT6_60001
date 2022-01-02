5285c9f67f4a4bc0f88d98376b121dc068c532b4
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-01-02 19:43:17 +0100

    Add function definition and and specification

---
+++ finger_exercises/get_days_between_canadian_and_american_thanksgiving.py
@@ -0,0 +1,4 @@
+def get_days_between_canadian_and_american_thanksgiving(year: int):
+  """year a number >= 1941
+  returns the number of days between the canadian and the american thanksgiving
+  in year"""
