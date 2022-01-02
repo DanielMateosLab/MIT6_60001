8ca448bf1c86a0766fdfeda7aa7a3aaa0bf7e3be
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-01-02 20:00:59 +0100

    Add get_day_number_of_nth_weekday_month utility function

--- finger_exercises/get_days_between_canadian_and_american_thanksgiving.py
+++ finger_exercises/get_days_between_canadian_and_american_thanksgiving.py
@@ -1,4 +1,14 @@
+import calendar
+
 def get_days_between_canadian_and_american_thanksgiving(year: int):
   """year a number >= 1941
   returns the number of days between the canadian and the american thanksgiving
   in year"""
+
+def get_day_number_of_nth_weekday_of_month(nth: int, weekday: int, month: int, year: int):
+  weeks_of_month = calendar.monthcalendar(year, month)
+  first_week_has_weekday = weeks_of_month[0][weekday]
+
+  return (first_week_has_weekday
+    and weeks_of_month[nth -1][weekday]
+    or weeks_of_month[nth][weekday])
