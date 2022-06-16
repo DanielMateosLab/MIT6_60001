00dbae577317b66ddb2ce31c2ad921f9335ae54a
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-06-16 08:58:35 +0200

    Write tests for update_hand()

--- PS3/test_extra.py
+++ PS3/test_extra.py
@@ -1,4 +1,4 @@
-from ps3 import get_letter_points, get_multiplier
+from ps3 import get_letter_points, get_multiplier, update_hand
 import pytest
 
 
@@ -12,3 +12,14 @@ def test_get_letter_points(test_input, expected):
 @pytest.mark.parametrize("word_len,hand_len,expected", [(4, 6, 22), (2, 7, 1)])
 def test_get_multiplier(word_len, hand_len, expected):
     assert get_multiplier(word_len, hand_len) == expected
+
+
+@pytest.mark.parametrize(
+    "hand,word,expected_hand",
+    [
+        ({"a": 2, "u": 1, "c": 3, "o": 1}, "launch", {"a": 1, "c": 2, "o": 1}),
+        ({"e": 1, "u": 1, "c": 3, "o": 1}, "weed", {"u": 1, "c": 3, "o": 1}),
+    ],
+)
+def test_update_hand(hand, word, expected_hand):
+    assert update_hand(hand, word) == expected_hand
